548117e0fb452d11c56eeac83cd33c07c6d220cc
chko <Charalampos.Kominos@enea.com>
2016-12-16 16:07:03 +0100

    identation fix

--- LoadSudoku.c
+++ LoadSudoku.c
@@ -23,29 +23,29 @@ int* LoadSudoku(int *ptr_sudoku_size){
     //strip newline character
     char *pos;
     if ((pos=strchr(buffer, '\n')) != NULL)*pos = '\0';
-    FILE *fp ;	
+    FILE *fp ;  
     
     if((fp=fopen(buffer,"r"))==NULL) {
         fprintf(stderr,"Error opening file %s",buffer);
         getchar();
         }
     else{
-       	while (fgets(buffer,256, fp) != NULL){   				
-    	    if ((pos=strchr(buffer, '\n')) != NULL)*pos = '\0';
-    	    len = strlen(buffer);
+        while (fgets(buffer,256, fp) != NULL){                  
+            if ((pos=strchr(buffer, '\n')) != NULL)*pos = '\0';
+            len = strlen(buffer);
             *ptr_sudoku_size=len;
             if (firstreadflag==0){
-	            Board=(int *) malloc(len*len*sizeof(int));
-    		    firstreadflag=1;
-    		}    			
-    		for(i=0;i<len;i++){
-    			Board[linenumber*len+i]=(int)(buffer[i]-'0');    				
-    	 	}
-    	 	linenumber++;		
-		}
-	printf("File loaded succesfully ");			
-	}
-	fclose(fp);
-	return Board;
+                Board=(int *) malloc(len*len*sizeof(int));
+                firstreadflag=1;
+            }               
+            for(i=0;i<len;i++){
+                Board[linenumber*len+i]=(int)(buffer[i]-'0');                   
+            }
+            linenumber++;       
+        }
+    printf("File loaded succesfully ");         
+    }
+    fclose(fp);
+    return Board;
 }
 
--- PrintSudoku.c
+++ PrintSudoku.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 void PrintSudoku(int* Board,int sudoku_size){
-	int linenumber=0,i,j;
+    int linenumber=0,i,j;
     for(i=0;i<sudoku_size;i++){
         for(j=0;j<sudoku_size;j++){
-    				printf("%d",Board[(linenumber*sudoku_size+j)]);    								
-    	}
+                    printf("%d",Board[(linenumber*sudoku_size+j)]);                                 
+        }
         linenumber++;
-    	printf("\n");		
-	}
+        printf("\n");       
+    }
 }
